1d10018331c52fafe8085f085ee247453d5a1790
Eduardo <miltormentas@tutanota.com>
2023-07-25 12:31:58 -0400

    Created Project

---
+++ Higher Lower Game/Higher Lower Game/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Higher_Lower_Game.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
